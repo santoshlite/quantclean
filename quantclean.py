8820e968e295404632ae2a04e986108bac221cf6
Santosh Passoubady <61618641+ssantoshp@users.noreply.github.com>
2021-05-02 23:44:59 +0200

    Update quantclean.py

--- quantclean.py
+++ quantclean.py
@@ -1,8 +1,12 @@
 import pandas as pd
 from pandas_datareader import data as web
+import logging
 
-def sweep(data):
+def sweeper(data):
+  for name in logging.Logger.manager.loggerDict.keys():
+    logging.getLogger(name).setLevel(logging.CRITICAL)
 
+  #non efficient, right?
   data.columns = ['Open' if 'open' in x else 'Open' if 'OPEN' in x else x for x in data.columns]
 
   data.columns = ['High' if 'high' in x else 'High' if 'HIGH' in x else x for x in data.columns]
@@ -23,15 +27,35 @@ def sweep(data):
   elif 'Time' in data.columns and not 'Date' in data.columns:
     data['Date'] = data['Time']
 
-  else:
-    data['Date'] = data.index
+  elif 'Date' in data.columns and not 'Time' in data.columns:
+    pass
 
-  df = data[['Date','Open','High','Low', 'Close', 'Volume']]
 
-  df.reset_index(drop=True, inplace=True)
+  try:
+    df = data[['Date','Open','High','Low', 'Close', 'Volume']]
+    df.reset_index(drop=True, inplace=True)
+    df['Date'] = df['Date'].astype(str)
+    df['Date'] = df['Date'].str.replace(r'-|/', '')
+    return df
 
-  df['Date'] = df['Date'].astype(str)
-  df['Date'] = df['Date'].str.replace(r'-|/', '')
-  df 
+  except KeyError:
 
-  return df
+    #non sense messages
+    print('Oupsi...seems like someone has cast a spell on your dataset')
+
+    print('Checking which column has been bewitched...')
+
+    cols = ['Date','Open','High','Low', 'Close', 'Volume']
+
+    for col in cols:
+      if col not in data.columns:
+        print(col + " is invisible")
+        data[col] = ""
+
+    print("The spell has been successfuly broken!")
+
+    df = data[['Date','Open','High','Low', 'Close', 'Volume']]
+    df.reset_index(drop=True, inplace=True)
+    df['Date'] = df['Date'].astype(str)
+    df['Date'] = df['Date'].str.replace(r'-|/', '')
+    return df
